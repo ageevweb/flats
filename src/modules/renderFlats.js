342fae42c24b264036d883349f61ac330580c413
Andrey Ageev <ageev@MacBook-Pro-Andrey.local>
2020-12-15 15:10:17 +0300

    animation modal

--- src/modules/renderFlats.js
+++ src/modules/renderFlats.js
@@ -6,19 +6,21 @@ export function renderFlats(flats) {
   let flatsArr = flats.map((item) => {
     return `
       <div class="flat-item">
-        <div class="flat-item__top">
-          <div class="flat-item__level">${item.level} этаж</div>
-          <div class="flat-item__type">${item.type}</div>
-          <div class="flat-item__meters">${item.meters} м<sup>2</sup></div>
+        <div class="flat-item__wrapper">
+          <div class="flat-item__top">
+            <div class="flat-item__level">${item.level} этаж</div>
+            <div class="flat-item__type">${item.type}</div>
+            <div class="flat-item__meters">${item.meters} м<sup>2</sup></div>
+          </div>
+          <div class="flat-item__img" style="background-image: url('img/${item.img}.jpg')">
+            <div class="flat-item__number">№${item.id}</div>
+          </div>
+          <div class="flat-item__bottom">
+            <div class="flat-item__price">${prettify(item.price)} р.</div>
+            <div class="flat-item__for-meter">${prettify(Math.floor(item.price / item.meters))} р. за м<sup>2</sup></div>
+          </div>
+          <button class="flat-item__more btn" data-id="${item.id}" >подробнее</button>
         </div>
-        <div class="flat-item__img" style="background-image: url('img/${item.img}.jpg')">
-          <div class="flat-item__number">№${item.id}</div>
-        </div>
-        <div class="flat-item__bottom">
-          <div class="flat-item__price">${prettify(item.price)} р.</div>
-          <div class="flat-item__for-meter">${prettify(Math.floor(item.price / item.meters))} р. за м<sup>2</sup></div>
-        </div>
-        <button class="flat-item__more btn" data-id="${item.id}" >подробнее</button>
       </div>
     `
   })
